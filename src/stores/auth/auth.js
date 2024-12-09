9c42b63fb843123cdb15fe4e14c395091f153641
GabrielLima2803 <gabriellima2803@gmail.com>
2024-12-09 17:06:41 -0300

    feat: Criação das stores

---
+++ src/stores/auth/auth.js
@@ -0,0 +1,91 @@
+import { defineStore } from "pinia";
+import { computed, reactive } from "vue";
+import { useLoadingStore } from '../others/loading';
+import AuthService from '@/services';
+
+const authService = new AuthService();
+
+export const useAuthStore = defineStore('auth', () => {
+    const state = reactive({
+        token: localStorage.getItem('authToken') || null,
+    })
+
+    const loadingStore = useLoadingStore()
+    const isLogged = computed(() => !!state.token)
+
+    function setToken(newToken) {
+        state.token = newToken;
+        localStorage.setItem('authToken', newToken);
+      }
+
+    function LogoutUser() {
+        loadingStore.startLoading();
+        state.token = null
+        isLogged.value = false
+        localStorage.removeItem('authToken')
+        loadingStore.stopLoading();
+    }
+
+    async function RegisterEmpresa(empresaData) {
+        loadingStore.startLoading();
+        try {
+          const response = await authService.RegisterEmpresa(empresaData);
+          return response;
+        } catch (error) {
+          console.error('Erro no registro:', error);
+          throw error;
+        } finally {
+          loadingStore.stopLoading();
+        }
+      }
+
+      async function LoginEmpresa(credentials) {
+        loadingStore.startLoading();
+        try {
+          const response = await authService.LoginEmpresa(credentials);
+          setToken(response.access);
+          return response;
+        } catch (error) {
+          console.error('Erro no login:', error);
+          throw error;
+        } finally {
+          loadingStore.stopLoading();
+        }
+      }
+
+      async function ForgotPasswordEmpresa(email) {
+        loadingStore.startLoading();
+        try {
+          const response = await authService.ForgotPasswordEmpresa(email);
+          return response;
+        } catch (error) {
+          console.error('Erro ao solicitar recuperação de senha:', error);
+          throw error;
+        } finally {
+          loadingStore.stopLoading();
+        }
+      }
+    
+      async function ResetPasswordEmpresa(resetData) {
+        loadingStore.startLoading();
+        try {
+          const response = await authService.ResetPasswordEmpresa(resetData); 
+          return response;
+        } catch (error) {
+          console.error('Erro ao resetar a senha:', error);
+          throw error;
+        } finally {
+          loadingStore.stopLoading();
+        }
+      }
+
+    return {
+        isLogged,
+        LogoutUser,
+        RegisterEmpresa,
+        LoginEmpresa,
+        ForgotPasswordEmpresa,
+        ResetPasswordEmpresa,
+    }
+
+})
---
+++ src/stores/others/empresa.js
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import { computed, reactive } from "vue";
+import { useLoadingStore } from "./loading";
+import EmpresaService from '../../services/others/empresa';
+
+const empresaService = new EmpresaService();
+
+export const useEmpresaStore = defineStore('empresas', () => {
+    const state = reactive({
+        currentEmpresa: []
+    })
+
+    const currentEmpresa = computed(() => state.currentEmpresa)
+
+    const loadingStore = useLoadingStore();
+
+    const getMeEmpresa = async (token) => {
+        loadingStore.startLoading();
+        try {
+            const data = await empresaService.getMeEmpresa(token);  
+            state.currentUser = data;  
+          } catch (error) {
+            console.error('Erro ao buscar o usuário logado:', error);
+          } finally {
+            loadingStore.stopLoading();
+          }
+    }
+
+    return {
+        currentEmpresa,
+        getMeEmpresa
+    }
+})
