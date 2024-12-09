9e820bfb4aeae86ebbf3f073720a31a563571501
GabrielLima2803 <gabriellima2803@gmail.com>
2024-12-09 14:18:54 -0300

    feat: Criação dos components ForgotPassword

---
+++ src/components/ForgotPasswod/DefaultForgotPasswod.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ForgotPasswodDesktop from './Desktop/ForgotPasswodDesktop.vue';
+import ForgotPasswodMobile from './Mobile/ForgotPasswodMobile.vue';
+
+import { useScreen } from '@/composables';
+
+const {isMobile} = useScreen()
+</script>
+
+<template>
+    <ForgotPasswodMobile v-if="isMobile"/>
+    <ForgotPasswodDesktop v-else/>
+</template>
+
+<style scoped>
+</style>
