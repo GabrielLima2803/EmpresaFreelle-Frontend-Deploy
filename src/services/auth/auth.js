c33f99d7cef50c47e89e287353f7a23a3756a4ec
GabrielLima2803 <gabriellima2803@gmail.com>
2024-12-09 17:06:20 -0300

    feat: Criação de services

---
+++ src/services/auth/auth.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+class AuthService {
+     async RegisterEmpresa(empresaData) {
+        try {
+            const response = await axios.post("register/", empresaData);
+            return response.data;
+        } catch (error) {
+            console.error('API request error:', error); 
+            throw error;
+        }
+    }
+
+    async LoginEmpresa(credentials) {
+        try {
+            const response = await axios.post("login/", credentials);
+            return response.data;
+        } catch (error) {
+            console.error('API request error:', error); 
+            throw error;
+        }
+    }
+
+    async ForgotPasswordEmpresa(email) {
+        try {
+            const response = await axios.post("forgot-password/", { email });
+            return response.data;
+        } catch (error) {
+            console.error('API request error:', error); 
+            throw error;
+        }
+    }
+    async ResetPasswordEmpresa(resetData) {
+        try {
+            const response = await axios.post("reset-password/", resetData);
+            return response.data;
+        } catch (error) {
+            console.error('API request error:', error); 
+            throw error;
+        }
+    }
+}
+
+export default AuthService;
---
+++ src/services/others/empresa.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+class EmpresaService {
+    async getAllEmpresas() {
+        try {
+            const response = await axios.get("usuarios/empresas/");
+            return response.data;
+        } catch (error) {
+            console.error('API request error:', error); 
+            throw error;
+        }
+    }
+    async getMeEmpresa(authToken) {
+        try {
+            const response = await axios.get('/usuarios/me', {
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Erro ao buscar o usuário logado:", error);
+            throw error;  
+        }
+    }
+
+}
+ 
+export default EmpresaService();
