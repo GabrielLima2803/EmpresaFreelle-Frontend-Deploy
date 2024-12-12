fa74c324a1acfda50deb626c86bccf799de206c8
GabrielLima2803 <gabriellima2803@gmail.com>
2024-12-12 19:41:25 -0300

    feat: Criação do CardRequest

---
+++ src/components/MainComponents/Header/Card/CardRequest.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="box-img">
+            <img src="https://via.placeholder.com/150" alt="">
+        </div>
+        <div class="about">
+            <h1>Ana Clara Gomes</h1>
+            <p>Email</p>
+            <p>Trabalho</p>
+        </div>
+        <div>
+            <button>Aceitar</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
