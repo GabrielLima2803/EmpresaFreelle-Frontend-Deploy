49af43d8943f5216e27a44e34d8c1f908c5c7068
GabrielLima2803 <gabriellima2803@gmail.com>
2024-12-12 19:42:11 -0300

    feat: Removendo o Bootstrap

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import router from './router'
 import './plugins/axios';
 
 import '@mdi/font/css/materialdesignicons.min.css';
-
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap';
 
 const app = createApp(App)
 
